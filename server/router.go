76ca7dc03dfb0f4eae27425fcca2ac9e5ec577bd
rBad Chicken <romanjordan688@gmail.com>
2022-10-10 14:21:13 +0000

    INIT

--- server/router.go
+++ server/router.go
@@ -15,8 +15,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func Router(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		SetHeaders(w)
 		if strings.HasSuffix(r.URL.Path, "/") {
+			SetHeaders(w)
 			// w.Header().Set("content-type","text/html; charset=utf-8")
 			status.S_404(w)
 			return
